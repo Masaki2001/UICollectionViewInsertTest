3d14cc9f82c80040bf05dcb451c9f34d95cddde7
Masaki Murakami <yvzbtbcug@gmail.com>
2020-02-07 14:57:14 +0900

    Initial Commit

---
+++ UICollectionViewInsertTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewInsertTest
+//
+//  Created by Masaki Murakami on 2020/02/07.
+//  Copyright © 2020 Masaki Murakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
